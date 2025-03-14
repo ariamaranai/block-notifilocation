1faefb55065a282f5bd7b9fbfed7188f176a5df3
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2025-03-14 09:22:43 +0900

    Add files via upload

---
+++ bg.js
@@ -0,0 +1,10 @@
+chrome.runtime.onInstalled.addListener(() => (
+  chrome.contentSettings.notifications.set({
+    primaryPattern: "<all_urls>",
+    setting: "block"
+  }),
+  chrome.contentSettings.location.set({
+      primaryPattern: "<all_urls>",
+      setting: "block"
+  })
+));
