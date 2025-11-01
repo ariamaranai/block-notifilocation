f36fcf9f470d8d6c620f57130205d39931792e0d
a <106607480+ariamaranai@users.noreply.github.com>
2025-11-02 02:29:59 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -1,6 +1,6 @@
 chrome.runtime.onInstalled.addListener(() => {
-  let contentSettings = chrome.contentSettings;
+  let { notifications, location } = chrome.contentSettings;
   let value = { primaryPattern: "<all_urls>", setting: "block" };
-  contentSettings.notifications.set(value);
-  contentSettings.location.set(value);
+  notifications.set(value);
+  location.set(value);
 });
